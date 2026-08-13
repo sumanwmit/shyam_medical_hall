e19efa7dd97ce834e65e6c7b112aede7e6aee688
sumanwmit <suman.wmit@gmail.com>
2026-08-13 09:42:52 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer: React.FC<FooterProps> = ({ onOpenWhatsAppModal }) => {
 
   // Global Tracking Hook required by specification
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
